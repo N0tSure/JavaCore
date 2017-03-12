738225c87397e8c408925965cb2e74381c516475
NotSure <ASir2089@gmail.com>
2017-03-13 01:50:27 +0300

    Map test class added, and also demonstration pseudo map class added

---
+++ src/main/java/ekkel/book/containers/MapTest.java
@@ -0,0 +1,94 @@
+package ekkel.book.containers;
+
+import ekkel.book.util.TextFile;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by cresh on 12.03.17.
+ */
+public class MapTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapTest.class);
+
+    @Test
+    public void wordCounter() throws Exception {
+        TextFile textFile = new TextFile("./src/main/resources/asci.sample", "\\W+");
+        LOGGER.info("All words: {}", textFile);
+
+        AssociativeArray<String, Integer> counter = new AssociativeArray<>(textFile.size());
+
+        for (String word : textFile) {
+            Integer count = counter.get(word);
+            if (count == null)
+                counter.put(word, 1);
+            else {
+                counter.put(word, count+1);
+            }
+        }
+
+        LOGGER.info("Result: {}", counter);
+
+    }
+
+    @Test
+    public void associativeTest() {
+        AssociativeArray<String, String> map = new AssociativeArray<>(6);
+        map.put("sky", "blue");
+        map.put("grass", "green");
+        map.put("ocean", "dancing");
+        map.put("tree", "tall");
+        map.put("earth", "brown");
+        map.put("sun", "warm");
+        try {
+            map.put("extra", "object"); // Past the end
+        } catch(ArrayIndexOutOfBoundsException e) {
+            LOGGER.error("Too many objects!", e);
+        }
+        LOGGER.info(map.toString());
+        LOGGER.info(map.get("ocean"));
+    }
+
+    @Test
+    public void maps() throws Exception {
+        mapsTest(new HashMap<>());
+        mapsTest(new TreeMap<>());
+        mapsTest(new LinkedHashMap<>());
+        mapsTest(new IdentityHashMap<>());
+        mapsTest(new ConcurrentHashMap<>());
+        mapsTest(new WeakHashMap<>());
+
+    }
+
+    private void mapsTest(Map<Integer, String> map) {
+        LOGGER.info(map.getClass().getSimpleName());
+        map.putAll(new CountingMapData(25));
+        // Map has 'Set' behavior for keys:
+        map.putAll(new CountingMapData(25));
+        printKeys(map);
+        // Producing a Collection of the values:
+        LOGGER.info("Values: {}", map.values());
+        LOGGER.info(map.toString());
+        LOGGER.info("map.containsKey(11): " + map.containsKey(11));
+        LOGGER.info("map.get(11): " + map.get(11));
+        LOGGER.info("map.containsValue(\"F0\"): " + map.containsValue("F0"));
+        Integer key = map.keySet().iterator().next();
+        LOGGER.info("First key in map: " + key);
+        map.remove(key);
+        printKeys(map);
+        map.clear();
+        LOGGER.info("map.isEmpty(): " + map.isEmpty());
+        map.putAll(new CountingMapData(25));
+        // Operations on the Set change the Map:
+        map.keySet().removeAll(map.keySet());
+        LOGGER.info("map.isEmpty(): " + map.isEmpty());
+    }
+
+    private void printKeys(Map<Integer, String> map) {
+        LOGGER.info("Size = {}, Keys: {}", map.size(), map.keySet());
+    }
+}
