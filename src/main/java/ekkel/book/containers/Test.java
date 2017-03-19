619c584219a43761adba27edb4f508a8f9b2a5ef
NotSure <ASir2089@gmail.com>
2017-03-20 01:48:56 +0300

    Test framework was created

---
+++ src/main/java/ekkel/book/containers/Test.java
@@ -0,0 +1,18 @@
+package ekkel.book.containers;
+
+/**
+ * Created by cresh on 19.03.17.
+ */
+abstract class Test<C> {
+    private String name;
+
+    public Test(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    abstract int test(C container, TestParam testParam);
+}
