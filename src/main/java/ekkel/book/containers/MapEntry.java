01e52dffbc61a66d9edd66aded441f53a7961c2a
NotSure <ASir2089@gmail.com>
2017-03-19 18:48:37 +0300

    MapEntry refactored as linked list

--- src/main/java/ekkel/book/containers/MapEntry.java
+++ src/main/java/ekkel/book/containers/MapEntry.java
@@ -8,10 +8,35 @@ import java.util.Map;
 class MapEntry<K, V> implements Map.Entry<K, V> {
     private K key;
     private V value;
+    private MapEntry<K, V> next;
 
     MapEntry(K key, V value) {
         this.key = key;
         this.value = value;
+    }
+
+    public MapEntry(K key, V value, MapEntry<K, V> next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    public MapEntry() {
+        key = null;
+        value = null;
+        next = null;
+    }
+
+    boolean hasNext() {
+        return (key != null) && (value != null) && (next != null);
+    }
+
+    MapEntry<K, V> getNext() {
+        return next;
+    }
+
+    void setNext(MapEntry<K, V> next) {
+        this.next = next;
     }
 
     @Override
