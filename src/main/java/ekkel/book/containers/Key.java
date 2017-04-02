b6e67642913e93fd065131d8bc87983ec1d1e2bf
NotSure <ASir2089@gmail.com>
2017-04-02 15:58:18 +0300

    Java Reference API was studied

---
+++ src/main/java/ekkel/book/containers/Key.java
@@ -0,0 +1,10 @@
+package ekkel.book.containers;
+
+/**
+ * Created by cresh on 02.04.17.
+ */
+class Key extends Element {
+    public Key(String id) {
+        super(id);
+    }
+}
---
+++ src/main/java/ekkel/book/containers/Value.java
@@ -0,0 +1,10 @@
+package ekkel.book.containers;
+
+/**
+ * Created by cresh on 02.04.17.
+ */
+class Value extends Element {
+    public Value(String id) {
+        super(id);
+    }
+}
