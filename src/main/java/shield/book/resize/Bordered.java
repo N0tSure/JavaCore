a1f30f10eb3b1bb023055e2e4f72ff5383c60ccd
N0tSure <ASir2089@gmail.com>
2017-05-17 12:17:30 +0300

    Image resizing simulation created

--- src/main/java/shield/book/resize/Bordered.java
+++ src/main/java/shield/book/resize/Bordered.java
@@ -5,18 +5,14 @@ package shield.book.resize;
  *
  * @author Artemis A. Sirosh
  */
-class Bordered implements Image {
+class Bordered extends AbstractImage {
 
-    private static final char DARK = 'H';
-    private static final char LIGHT = '_';
-
-    private int width;
-    private int height;
-    private char[][] body;
+    public Bordered(int width, int height) {
+        super(width, height);
+    }
 
     public Bordered(int width, int height, int wall) {
-        this.width = width;
-        this.height = height;
+        super(width, height);
 
         if (wall >= width || wall >= height)
             throw new IllegalArgumentException("Wall cannot be wider than square");
@@ -26,7 +22,7 @@ class Bordered implements Image {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
                 boolean isHole = ((i >= wall && j >= wall) && (i < width - wall && j < height - wall));
-                body[i][j] = isHole ? Bordered.LIGHT : Bordered.DARK;
+                body[i][j] = isHole ? Image.LIGHT : Image.DARK;
             }
         }
     }
@@ -48,20 +44,4 @@ class Bordered implements Image {
         body[x][y] = pix;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-
-        builder.append(String.format("Size: %dx%d\n", width, height));
-
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                builder.append(body[i][j]);
-                builder.append(' ');
-            }
-            builder.append('\n');
-        }
-
-        return builder.toString();
-    }
 }
