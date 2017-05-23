dc89bf36e34168ebcd6456cbaea1086a1056be1c
N0tSure <ASir2089@gmail.com>
2017-05-23 17:55:08 +0300

    * TreeInfo represents information about file tree

---
+++ src/main/java/ekkel/book/io/TreeInfo.java
@@ -0,0 +1,30 @@
+package ekkel.book.io;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created on 23.05.2017.
+ * Information about directory tree
+ * @author Artemis A. Sirosh
+ */
+public interface TreeInfo extends Iterable<File> {
+
+    /**
+     * Return all files from directory tree
+     * @return list of directories
+     */
+    List<File> getFiles();
+
+    /**
+     * Return all directories from directory tree
+     * @return list of files
+     */
+    List<File> getDirectories();
+
+    /**
+     * Add other {@code TreeInfo} to this TreeInfo
+     * @param other TreeInfo object
+     */
+    void addAll(TreeInfo other);
+}
