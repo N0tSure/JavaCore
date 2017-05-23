c3078d4c5c59c273da07ed275d4f15f1882373b8
N0tSure <ASir2089@gmail.com>
2017-05-23 17:56:27 +0300

    * Utility for convert large collections to string

---
+++ src/main/java/ekkel/book/util/ContainerToString.java
@@ -0,0 +1,28 @@
+package ekkel.book.util;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created on 23.05.2017.
+ *
+ * @author Artemis A. Sirosh
+ */
+public class ContainerToString {
+    public static String format(Collection<?> collection) {
+        if (collection.isEmpty() || collection.size() == 1)
+            return collection.toString();
+
+        StringBuilder builder = new StringBuilder("[\n");
+
+
+        for (Object item : collection)
+            builder.append(item).append('\n');
+
+        return builder.append("\n]").toString();
+    }
+
+    public static String format(Object[] objects) {
+        return format(Arrays.asList(objects));
+    }
+}
