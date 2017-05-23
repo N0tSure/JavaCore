c5666523d08fa3643b79f58a20fd08edaf56b4a5
N0tSure <ASir2089@gmail.com>
2017-05-23 13:30:02 +0300

    * created class for listing files in sorted order

---
+++ src/main/java/ekkel/book/io/SortedDirectoryList.java
@@ -0,0 +1,43 @@
+package ekkel.book.io;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkArgument;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Created on 23.05.2017.
+ *
+ * @author Artemis A. Sirosh
+ */
+public class SortedDirectoryList {
+
+    private final File directory;
+
+    public SortedDirectoryList(File directory) {
+        checkArgument(directory.exists(), String.format("%s not exists", directory.getName()));
+        this.directory = directory;
+    }
+
+    public SortedDirectoryList(String directoryName) {
+        this(new File(checkNotNull(directoryName, "Directory must be not null")));
+    }
+
+    public List<File> list() {
+        return fileCreator(directory.list());
+    }
+
+    public List<File> list(String regexp) {
+        Pattern pattern = Pattern.compile(regexp);
+        return fileCreator(directory.list((dir, fileName) -> pattern.matcher(fileName).matches()));
+    }
+
+    private List<File> fileCreator(String[] fileNames) {
+        Arrays.sort(fileNames);
+        return Arrays.stream(fileNames).map(fileName -> new File(directory, fileName)).collect(Collectors.toList());
+    }
+}
