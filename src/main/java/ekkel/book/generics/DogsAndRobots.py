a82b74cf5489a9c6e64de8749cea80a7009e8f01
N0tSure <ASir2089@gmail.com>
2017-05-25 17:04:15 +0300

    * reformatted for 3.x interpreter version

--- src/main/java/ekkel/book/generics/DogsAndRobots.py
+++ src/main/java/ekkel/book/generics/DogsAndRobots.py
@@ -1,29 +1,38 @@
 class Dog:
-	def speak(self):
-		print "Arf!"
-	def sit(self):
-		print "Sitting"
-	def reproduce(self):
-		pass
-		
+    def speak(self):
+        print('Arf!')
+
+    def sit(self):
+        print("Sitting")
+
+    def reproduce(self):
+        pass
+
+
 class Robot:
-	def speak(self):
-		print "Click!"
-	def sit(self):
-		print "Clank!"
-	def oilChange(self):
-		pass
+    def speak(self):
+        print("Click!")
+
+    def sit(self):
+        print("Clank!")
+
+    def oil_change(self):
+        pass
+
 
 class Mime:
-	def sit(self):
-		print "Pretend to sit"
-	def pushInvisibleWall(self):
-		pass
-		
+    def sit(self):
+        print("Pretend to sit")
+
+    def push_invisible_wall(self):
+        pass
+
+
 def perform(anything):
-	anything.speak()
-	anything.sit()
-	
+    anything.speak()
+    anything.sit()
+
+
 a = Dog()
 b = Robot()
 c = Mime()
