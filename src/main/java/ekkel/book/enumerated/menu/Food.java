59a37570a6574cea202ea4fee562e03faae9bfc8
N0tSure <ASir2089@gmail.com>
2018-12-15 00:34:51 +0300

    Create interface collecting food types

---
+++ src/main/java/ekkel/book/enumerated/menu/Food.java
@@ -0,0 +1,29 @@
+package ekkel.book.enumerated.menu;
+
+/**
+ * Created on 14 Dec, 2018.
+ *
+ * @author Artemis A. Sirosh
+ */
+public interface Food {
+
+    enum Appetizer implements Food {
+        SALAD, SOUP, SPRING_ROLLS
+    }
+
+    enum MainCourse implements Food {
+        LASAGNE, BURRITO, PAD_THAI,
+        LENTILS, HUMMOUS, VINDALOO
+    }
+
+    enum Dessert implements Food {
+        TIRAMISU, GELATO, BLACK_FOREST_CAKE,
+        FRUIT, CREME_CARAMEL
+    }
+
+    enum Coffee implements Food {
+        BLACK_COFFEE, DECAF_COFFEE, ESPRESSO,
+        LATTE, CAPPUCCINO, TEA, HERB_TEA
+    }
+
+}
