d3e18835f3d7a755c94f9aa811819ecac31ccff4
N0tSure <ASir2089@gmail.com>
2019-01-05 22:24:32 +0300

    Add AlarmPoints

---
+++ src/main/java/ekkel/book/enumerated/AlarmPoints.java
@@ -0,0 +1,10 @@
+package ekkel.book.enumerated;
+
+/**
+ * Created on 05 Jan, 2019.
+ *
+ * @author Artemis A. Sirosh
+ */
+public enum  AlarmPoints {
+    STAIR1, STAIR2, LOBBY, OFFICE1, OFFICE2, OFFICE3, OFFICE4, BATHROOM, UTILITY, KITCHEN
+}
