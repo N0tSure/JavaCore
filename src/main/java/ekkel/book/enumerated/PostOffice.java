9a116f00ceac30045a0ac8671ecb218eac5d6b99
N0tSure <ASir2089@gmail.com>
2019-01-06 23:19:37 +0300

    Remake PostOffice to use EnumMap

--- src/main/java/ekkel/book/enumerated/PostOffice.java
+++ src/main/java/ekkel/book/enumerated/PostOffice.java
@@ -3,7 +3,8 @@ package ekkel.book.enumerated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
 
 /**
  * Created on 06 Jan, 2019.
@@ -12,150 +13,98 @@ import java.util.Arrays;
  */
 class PostOffice {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(PostOffice.class);
+    private static final Logger LOGGER;
+    private static final EnumMap<MailHandler, Handler> HANDLERS;
+
+    static {
+        LOGGER = LoggerFactory.getLogger(PostOffice.class);
+        HANDLERS = new EnumMap<>(MailHandler.class);
+
+        HANDLERS.put(
+                MailHandler.GENERAL_DELIVERY,
+                mail -> {
+                    if (mail.getGeneralDelivery() == Mail.GeneralDelivery.YES) {
+                        LOGGER.info("Using general delivery for: {}.", mail);
+                        return true;
+                    }
+
+                    return false;
+                }
+        );
+
+        HANDLERS.put(
+                MailHandler.MACHINE_SCAN,
+                mail -> {
+                    final boolean canHandle = mail.getScannability() != Mail.Scannability.UNSCANNABLE &&
+                            mail.getForward() != Mail.Forward.YES && mail.getAddress() != Mail.Address.INCORRECT;
+
+                    if (canHandle) {
+                        LOGGER.info("Delivering {} automatically.", mail);
+                    }
+
+                    return canHandle;
+                }
+        );
+
+        HANDLERS.put(
+               MailHandler.VISUAL_INSPECTION,
+               mail -> {
+                   final boolean canHandle = mail.getReadability() != Mail.Readability.ILLEGIBLE &&
+                           mail.getForward() != Mail.Forward.YES && mail.getAddress() != Mail.Address.INCORRECT;
+
+                   if (canHandle) {
+                       LOGGER.info("Delivering {} normally.", mail);
+                   }
+
+                   return canHandle;
+               }
+        );
+
+        HANDLERS.put(
+                MailHandler.FORWARDING,
+                mail -> {
+                    final boolean canHandle = mail.getForward() == Mail.Forward.YES &&
+                            (mail.getScannability() != Mail.Scannability.UNSCANNABLE ||
+                                    mail.getReadability() != Mail.Readability.ILLEGIBLE) &&
+                            mail.getAddress() != Mail.Address.INCORRECT;
+
+                    if (canHandle)
+                        LOGGER.info("Forwarding {}.", mail);
+
+                    return canHandle;
+                }
+        );
+
+        HANDLERS.put(
+                MailHandler.RETURN_TO_SENDER,
+                mail -> {
+                    if (mail.getReturnAddress() != Mail.ReturnAddress.MISSING) {
+                        LOGGER.info("Returning {} to sender.", mail);
+                        return true;
+                    }
+
+                    return false;
+                }
+        );
+    }
 
     static MailHandler handler(final Mail mail) {
-        return Arrays.stream(MailHandler.values())
-                .filter(mailHandler -> mailHandler.handle(mail))
+        return HANDLERS.entrySet().stream()
+                .filter(entry -> entry.getValue().handle(mail))
+                .map(Map.Entry::getKey)
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException(mail + " is a dead letter."));
     }
 
+    interface Handler {
+        boolean handle(Mail mail);
+    }
+
     enum MailHandler {
-        GENERAL_DELIVERY {
-
-            @Override
-            boolean handle(Mail m) {
-                switch (m.getGeneralDelivery()) {
-                    case YES:
-                        LOGGER.info("Using general delivery for: {}.", m);
-                        return true;
-
-                    default:
-                        return false;
-                }
-            }
-        },
-        MACHINE_SCAN {
-
-            @Override
-            boolean handle(Mail m) {
-
-                final boolean result;
-                switch (m.getScannability()) {
-
-                    case UNSCANNABLE:
-                        result = false;
-                        break;
-
-                    default:
-                        switch (m.getForward()) {
-                            case YES:
-                                result = false;
-                                break;
-                            default:
-                                switch (m.getAddress()) {
-                                    case INCORRECT:
-                                        result = false;
-                                        break;
-                                    default:
-                                        LOGGER.info("Delivering {} automatically.", m);
-                                        result = true;
-                                }
-                        }
-                }
-
-                return result;
-            }
-        },
-        VISUAL_INSPECTION {
-
-            @Override
-            boolean handle(Mail m) {
-
-                final boolean result;
-                switch (m.getReadability()) {
-                    case ILLEGIBLE:
-                        result = false;
-                        break;
-
-                    default:
-                        switch (m.getForward()) {
-                            case YES:
-                                result = false;
-                                break;
-
-                            default:
-                                switch (m.getAddress()) {
-                                    case INCORRECT:
-                                        result = false;
-                                        break;
-
-                                    default:
-                                        LOGGER.info("Delivering {} normally.", m);
-                                        result = true;
-                                }
-                        }
-                }
-
-                return result;
-            }
-        },
-        FORWARDING {
-            @Override
-            boolean handle(Mail mail) {
-                final boolean result;
-                switch (mail.getForward()) {
-                    case YES:
-                        switch (mail.getScannability()) {
-                            case UNSCANNABLE:
-                                switch (mail.getReadability()) {
-                                    case ILLEGIBLE:
-                                        result = false;
-                                        break;
-                                    default:
-                                        switch (mail.getAddress()) {
-                                            case INCORRECT:
-                                                result = false;
-                                                break;
-                                            default:
-                                                LOGGER.info("Forwarding {}.", mail);
-                                                result = true;
-                                        }
-                                }
-                                break;
-
-                            default:
-                                switch (mail.getAddress()) {
-                                    case INCORRECT:
-                                        result = false;
-                                        break;
-                                    default:
-                                        LOGGER.info("Forwarding {}.", mail);
-                                        result = true;
-                                }
-                        }
-                        break;
-
-                    default:
-                        result = false;
-                }
-
-                return result;
-            }
-        },
-        RETURN_TO_SENDER {
-            boolean handle(Mail m) {
-                switch(m.getReturnAddress()) {
-                    case MISSING:
-                        return false;
-                    default:
-                        LOGGER.info("Returning {} to sender.", m);
-                        return true;
-                }
-            }
-        };
-
-        abstract boolean handle(Mail m);
+        GENERAL_DELIVERY,
+        MACHINE_SCAN,
+        VISUAL_INSPECTION,
+        FORWARDING,
+        RETURN_TO_SENDER
     }
 }
