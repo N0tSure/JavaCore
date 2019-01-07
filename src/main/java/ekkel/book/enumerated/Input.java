27e09926f0695f7d1314658bf7aa961ee0cd2c16
N0tSure <ASir2089@gmail.com>
2019-01-08 02:48:14 +0300

    Make Input interface
    Input has been made an interface. To Input added enum Category and
    method which return current item Category. Category as separate enum
    was removed.

--- src/main/java/ekkel/book/enumerated/Input.java
+++ src/main/java/ekkel/book/enumerated/Input.java
@@ -1,46 +1,16 @@
 package ekkel.book.enumerated;
 
-import java.util.Random;
-
 /**
  * Created on 07 Jan, 2019.
  *
  * @author Artemis A. Sirosh
  */
-enum Input {
+interface Input {
 
-    NICKEL(5), DIME(10), QUARTER(25), DOLLAR(100),
+    int amount();
+    Category category();
 
-    TOOTHPASTE(200), CHIPS(75), SODA(100), SOAP(50),
-
-    ABORT_TRANSACTION {
-        public int amount() {
-            throw new RuntimeException("ABORT.amount()");
-        }
-    },
-    STOP { // This must be the last instance.
-        public int amount() { // Disallow
-            throw new RuntimeException("SHUT_DOWN.amount()");
-        }
-    };
-
-    private static Random rand = new Random(47);
-
-    private final int value;
-
-    public static Input randomSelection() {
-        return values()[rand.nextInt(values().length - 1)];
-    }
-
-    Input(int value) {
-        this.value = value;
-    }
-
-    Input() {
-        this.value = 0;
-    }
-
-    int amount() {
-        return value;
+    enum Category {
+        MONEY, ITEM_SELECTION, QUIT_TRANSACTION, SHUT_DOWN
     }
 }
