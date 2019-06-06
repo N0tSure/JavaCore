500816a647c94835f66e3b09fb98a99e9abb958b
N0tSure <ASir2089@gmail.com>
2019-06-06 15:17:38 +0300

    Add exercises

--- concurrency/src/test/java/com/artemsirosh/tij/concurrency/LiftOffTest.java
+++ concurrency/src/test/java/com/artemsirosh/tij/concurrency/LiftOffTest.java
@@ -3,6 +3,9 @@ package com.artemsirosh.tij.concurrency;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 /**
@@ -11,6 +14,11 @@ import java.util.stream.Stream;
  * @author Artem Sirosh 'ASir2089@gmail.com'
  */
 class LiftOffTest {
+
+    private static void useExecutorService(ExecutorService executor, Stream<? extends Runnable> tasks) {
+        tasks.forEach(executor::execute);
+        executor.shutdown();
+    }
 
     @Test
     @DisplayName("Task work in main thread")
@@ -36,4 +44,31 @@ class LiftOffTest {
                 .forEach(Thread::start);
     }
 
+    @Test
+    @DisplayName("Run traceable task using SingleThreadExecutor")
+    void useSingleThreadExecutor() {
+        final ExecutorService executor = Executors.newSingleThreadExecutor();
+        final Stream<TraceableTask> traceableTaskStream = Stream.generate(TraceableTask::new).limit(5);
+
+        useExecutorService(executor, traceableTaskStream);
+    }
+
+    @Test
+    @DisplayName("Run traceable task in a five threads")
+    void useFixedThreadPool() {
+        final ExecutorService executor = Executors.newFixedThreadPool(5);
+        final Stream<TraceableTask> traceableTaskStream = Stream.generate(TraceableTask::new).limit(5);
+
+        useExecutorService(executor, traceableTaskStream);
+
+    }
+
+    @Test
+    @DisplayName("Run traceable tasks in a several threads")
+    void useCachedTreadPool() {
+        final ExecutorService executor = Executors.newCachedThreadPool();
+        final Stream<TraceableTask> traceableTaskStream = Stream.generate(TraceableTask::new).limit(5);
+
+        useExecutorService(executor, traceableTaskStream);
+    }
 }
